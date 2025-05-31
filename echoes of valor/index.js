4616eda94314d71f9893c42f2fe17db529ae8ddd
Alex2045-commits <abiolchini06@gmail.com>
2025-05-31 09:33:49 +0200

    Delete echoes of valor directory

--- echoes of valor/index.js
+++
@@ -1,3 +0,0 @@
-function full() {
-    window.open('index.html', '', 'fullscreen=yes, scrollbars=no, width=100%, height=100%');
-}
--- echoes of valor/livello0/cursore.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById("GameCanvas");
-canvas.addEventListener('click', function () {
-  if (game.player) {
-    game.player.handleMouseClick();
-  }
-});
-canvas.style.cursor = 'none';
